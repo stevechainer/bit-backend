b9b18a38e780725e733012494d83df056833bed1
steve980102 <steve980102@gmail.com>
2024-02-17 05:10:26 +0900

    Add inc/dec/getPts/getAllPts function.

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -11,4 +11,67 @@ const userSchema = new mongoose.Schema({
 
 const UserModel = mongoose.model("User", userSchema);
 
+// Function to get all pts values
+UserModel.getAllPts = async function () {
+  try {
+    const users = await this.find({}, { _id: 0, pts: 1 });
+    const ptsArray = users.map((user) => user.pts);
+    return ptsArray;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to get pts by user ID
+UserModel.getPtsByUserId = async function (user) {
+  try {
+    const data = await this.findOne({ user: user });
+    if (data) {
+      return data.pts;
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to increase pts by User ID
+UserModel.increaseByUserId = async function (user, pts) {
+  let currentPts = await this.getPtsByUserId(user);
+
+  try {
+    this.findOneAndUpdate({ user: user }, currentPts + pts)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to decrease pts by User ID
+UserModel.decreaseByUserId = async function (user, pts) {
+  let currentPts = await this.getPtsByUserId(user);
+
+  if (currentPts - pts < 0) {
+    return;
+  }
+
+  try {
+    this.findOneAndUpdate({ user: user }, currentPts - pts)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = UserModel;
